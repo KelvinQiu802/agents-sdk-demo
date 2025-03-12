896293b6ab6ae600714f3a0eb7db847d183508c2
KelvinQiu <kelvinqiu802@outlook.com>
2025-03-12 23:57:12 +0800

    feat: add more agents

--- main.py
+++ main.py
@@ -10,13 +10,21 @@ def main():
         base_url=os.getenv("OPENAI_BASE_URL"),
     )
 
+    english_agent = Agent(name="英文专家",
+        instructions="你是一个英文专家，擅长把中文翻译成英文",
+        model=OpenAIChatCompletionsModel(
+            model="gpt-4o-mini",
+            openai_client=external_client,
+        )
+    )
 
-    translation_agent = Agent(name="英文专家",
-     instructions="你是一个英文专家，擅长把中文翻译成英文",
+    french_agent = Agent(name="法语专家",
+        instructions="你是一个法语专家，擅长把中文翻译成法语",
         model=OpenAIChatCompletionsModel(
             model="gpt-4o-mini",
             openai_client=external_client,
         ),
+        handoffs=[english_agent]
     )
 
     writing_agent = Agent(name="中文专家",
@@ -25,10 +33,19 @@ def main():
             model="gpt-4o-mini",
             openai_client=external_client,
         ),
-        handoffs=[translation_agent]
+        handoffs=[english_agent, french_agent]
     )
 
-    result = Runner.run_sync(writing_agent, "写一首关于春天的诗,并且翻译成英文")
+    triage_agent = Agent(name="问题分类专家",
+        instructions="你是一个问题分类专家，擅长根据问题分类",
+        model=OpenAIChatCompletionsModel(
+            model="gpt-4o-mini",
+            openai_client=external_client,
+        ),
+        handoffs=[writing_agent, english_agent, french_agent]
+    )
+
+    result = Runner.run_sync(triage_agent, "把后面的话翻译成英文： 你好，我是一个人")
     print(result.final_output)
 
 if __name__ == "__main__":
